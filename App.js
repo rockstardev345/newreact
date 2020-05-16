b2cdc4b753238371c0b53506bb97f16b5fc8e003
Muhammad Atif <muhammadatif@ma.fios-router.home>
2020-05-16 16:17:15 -0400

    basic ui created

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const a = {name:"omar",phone:123456789,country:"brazil",address:{address1: "32 street gol" }} ;
+  console.log(a.name);
+  //a = 40 ;
+  //console.log(a);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.bluecontainer}></View>
+      <View style={styles.redcontainer}></View>
+      <Text>hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e74c3c',
+    flexDirection: 'row',
+    marginTop:20,
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  bluecontainer:{
+    backgroundColor: '#2980b9',
+    height:100,
+    width:100,
+  },
+  redcontainer:{
+    backgroundColor: '#e74c3c',
+    height:100,
+    width:400,
+  }
+
+});
